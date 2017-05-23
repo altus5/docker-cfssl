64faabad13bc4724d6aa4bdbdb6b7aa536e2655c
msato <msato@altus5.com>
2017-05-23 16:58:38 +0900

    クライアント証明のhostnameオプションの指定方法を変更

--- bin/gen_client_cert.sh
+++ bin/gen_client_cert.sh
@@ -9,14 +9,13 @@ basedir=$(cd $(dirname $0) && pwd)
 # generate member certificate and private key
 for conf in $(find $CLIENT_CONF_GLOB -type f)
 do
-  hosts=`jq -r -c '.hosts | @csv' $conf | sed -e 's/"//g'`
   cn=`jq -r  '.CN' $conf`
   cfssl gencert \
     -ca=$CA_PEM \
     -ca-key=$CA_KEY_PEM \
     -config=$CA_CONFIG_CONF \
     -profile=client \
-    -hostname="$hosts" \
+    -hostname="$cn" \
     $conf - \
     | cfssljson -bare $CLIENT_CERT_DIR/$cn
 done
